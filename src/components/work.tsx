f5bd12883f53de9671ba07ed931df77880ab34e4
Scott <scott.wares@yahoo.co.uk>
2024-10-17 21:04:45 +0100

    Updating missing div col formatting for final experience entry

--- src/components/work.tsx
+++ src/components/work.tsx
@@ -43,6 +43,7 @@ const Experience = () => {
         <div ref={compRef} className="max-w-full py-20 lg:py-44 px-4 md:px-16 lg:px-64 bg-gray-900 min-h-screen" id='Work'>
             <h1 className="text-3xl lg:text-4xl text-white font-bold mb-10 text-center">Experience</h1>
             <div className="max-w-7xl mx-auto w-full grid grid-cols-9 gap-y-10 px-2">
+                
                 {/* Experience 1 */}
                 <div className="col-span-9 lg:col-span-4 w-full h-full">
                     <div id="intro-Slider" className="w-full h-full p-2 md:pl-4">
@@ -83,6 +84,7 @@ const Experience = () => {
                     <div className="absolute w-3 h-3 rounded-full bg-white z-10 text-white text-center"></div>
                 </div>
 
+                { /* Experience 3*/}
                 <div className="col-span-9 lg:col-span-4 w-full h-full">
                     <div id="intro-Slider" className="w-full h-full p-2 md:pl-4">
                         <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">January 2021 - April 2022</time>
@@ -90,11 +92,15 @@ const Experience = () => {
                         <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">Focused on backend engineering for a project related to self-sovereign identity and decentralized ownership of medical records.</p>
                     </div>
                 </div>
-                <div className="relative col-span-0 lg:col-span-1 w-full h-full hidden lg:flex justify-center items-center">
+
+                {/* Vertical line between experiences (hidden on mobile) */}
+                <div className="relative hidden lg:flex col-span-0 lg:col-span-1 w-full h-full justify-center items-center">
                     <div className="h-full w-[1px] bg-[#1484da]"></div>
                     <div className="absolute w-3 h-3 rounded-full bg-white z-10 text-white text-center"></div>
                 </div>
-                <div className="col-span-9 lg:col-span-4 w-full h-full"></div>
+
+                {/* Empty column for spacing (only on large screens) */}
+                <div className="hidden lg:block col-span-0 lg:col-span-4 w-full h-full"></div>
             </div>
         </div>
 
